cbfb1f99e9d3a1cd131d6a9ffcf1d3219b3590da
mhadhbira <ranyamehadhbi@gmail.com>
2024-10-01 12:59:41 +0200

    Initial commit

---
+++ currency-exchange-service/src/main/java/com/first/microservices/currency_exchange_service/service/CurrencyExchangeService.java
@@ -0,0 +1,25 @@
+package com.first.microservices.currency_exchange_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.first.microservices.currency_exchange_service.entities.ExchangeValue;
+import com.first.microservices.currency_exchange_service.repositories.ExchangeValueRepository;
+
+@Service
+public class CurrencyExchangeService {
+	
+	@Autowired
+	private ExchangeValueRepository exchangeValueRepository;
+	
+	public ExchangeValue retrieveExchangeValue(String from, String to) {
+		return exchangeValueRepository.findByFromAndTo(from, to);
+	}
+
+	public ExchangeValue createEmployee(ExchangeValue exchangeValue) {
+		// TODO Auto-generated method stub
+		return exchangeValueRepository.save(exchangeValue);
+	}
+
+}
---
+++ currency-exchange-service/src/test/java/com/first/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.first.microservices.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = CurrencyExchangeServiceApplication.class)
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
